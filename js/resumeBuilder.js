ff66ace0098acd2610e2f14dff0419769503b5fa
SameerS <samant_sameer@hotmail.com>
2016-10-20 12:43:14 +0530

    Review changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,12 +3,12 @@ var bio = {
     "role": "Dragon warrior",
     "contacts": {
         "mobile": "555-55-55",
+        "email": "po@dragonmaster.com",
         "github": "theRealPo",
-        "location": "Jade Buddha Temple",
-        "email": "samant_sameer@hotmail.com"
+        "location": "Jade Buddha Temple"
     },
-    "welcomeMessage": "Po is friendly, kind, energetic, impulsive, good-natured, spunky, innocent, fun-loving, compassionate, smart, naive, loveable, and he never gives up. Though Po's real passion was for Kung Fu, he was reluctant to disappoint his father by revealing it. Despite having a mild and friendly personality, Po also developed a severe self-loathing, believing himself a failure for being both obese (but in reality he is a healthy weight for his species) and a species not known for a warrior tradition.",
-    "image": "images/panda_1x.jpg",
+    "welcomeMessage": "Po is friendly, kind, energetic, impulsive, good-natured, spunky, innocent, fun-loving, compassionate, smart, naive, loveable, and he never gives up.",
+    "biopic": "images/panda_1x.jpg",
     "skills": ["friendly", "kind", "energetic", "impulsive"]
 };
 
@@ -29,7 +29,7 @@ var work = {
 };
 
 var projects = {
-    "project": [{
+    "projects": [{
         "title": "Defeat of Tai Lung",
         "dates": "2008",
         "description": "The vengeful and treacherous snow leopard Tai Lung is headed to Jade palace, and Po defends everyone from the oncoming threat. Po puts his heart—and his girth—into the task, and the unlikely hero ultimately found that his greatest weaknesses turn out to be his greatest strengths",
@@ -47,31 +47,38 @@ var education = {
         "name": "The Jade Palace ",
         "location": "Jade Buddha Temple",
         "degree": "Dragon Warrior",
-        "majors": "Kung Fu Master",
+        "majors": ["Kung Fu Master"],
         "dates": "2008 - 2009",
         "url": "Jade.Palace.com"
     }, {
         "name": "Noodle shop of Mr. Ping",
         "location": "peace vallay",
         "degree": "bachelor",
-        "majors": "Noodle Making",
+        "majors": ["Noodle Making"],
         "dates": "2005 - 2006",
         "url": "noodle.shop.com"
+    }],
+
+    "onlineCourses": [{
+        "title": "Kung Fu master nanodegree",
+        "school": "Udacity",
+        "dates": "2006-2008",
+        "url": "Udacity.com"
     }]
 
 };
 
 
-bio.display = function () {
+bio.display = function() {
     var Name = HTMLheaderName.replace("%data%", bio.name);
     var Role = HTMLheaderRole.replace("%data%", bio.role);
-    var Image = HTMLbioPic.replace("%data%", bio.image);
+    var Image = HTMLbioPic.replace("%data%", bio.biopic);
     var Message = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 
     $("#header").prepend(Role).prepend(Name).append(Image, Message);
     $("#header").append(HTMLskillsStart);
 
-    bio.skills.forEach(function (skill) {
+    bio.skills.forEach(function(skill) {
         var Skills = HTMLskills.replace("%data%", skill);
         $("#skills").append(Skills);
     });
@@ -81,44 +88,53 @@ bio.display = function () {
     var Email = HTMLemail.replace("%data%", bio.contacts.email);
     var Github = HTMLcontactGeneric.replace("%contact%", "github").replace("%data%", bio.contacts.github);
     $("#footerContacts").append(Mobile, Email, Github);
+    $("#topContacts").append(Mobile,Email,Github);
 
 };
 
-education.display = function () {
-    education.schools.forEach(function (school) {
+education.display = function() {
+    education.schools.forEach(function(school) {
         $("#education").append(HTMLschoolStart);
 
         var Name = HTMLschoolName.replace("%data%", school.name);
         var Degree = HTMLschoolDegree.replace("%data%", school.degree);
         var Dates = HTMLschoolDates.replace("%data%", school.dates);
         var Location = HTMLschoolLocation.replace("%data%", school.location);
-        var Major = HTMLschoolMajor.replace("%data%", school.majors);
-        $(".education-entry:last").append(Name + Degree, Dates, Location, Major);
+        var Major = HTMLschoolMajor.replace("%data%", school.majors);        
+        $(".education-entry:last").append(Name + Degree, Dates, Location, Major, URL);
+    });
+     education.onlineCourses.forEach(function(course) {
+        $(".education-entry:last").append(HTMLonlineClasses);
+        var OnlineTitle = HTMLonlineTitle.replace("%data%", course.title);
+        var OnlineSchool = HTMLonlineSchool.replace("%data%", course.school);
+        var OnlineDates = HTMLonlineDates.replace("%data%", course.dates);
+        var OnlineURL = HTMLonlineURL.replace("%data%", course.url);        
+        $(".education-entry:last").append( OnlineTitle + OnlineSchool, OnlineDates, OnlineURL);
     });
 };
 
-work.display = function () {
-    work.jobs.forEach(function (job) {
+work.display = function() {
+    work.jobs.forEach(function(job) {
         $("#workExperience").append(HTMLworkStart);
 
         var Employer = HTMLworkEmployer.replace("%data%", job.employer);
         var Title = HTMLworkTitle.replace("%data%", job.title);
         var Dates = HTMLworkDates.replace("%data%", job.dates);
         var Description = HTMLworkDescription.replace("%data%", job.description);
-
-        $(".work-entry:last").append(Employer + Title, Dates, Description);
+        var Location = HTMLworkLocation.replace("%data%", job.location);
+        $(".work-entry:last").append(Employer + Title, Dates, Description,Location);
     });
 };
 
-projects.display = function () {
-    projects.project.forEach(function (item) {
+projects.display = function() {
+    projects.projects.forEach(function(item) {
         $("#projects").append(HTMLprojectStart);
         var Title = HTMLprojectTitle.replace("%data%", item.title);
         var Dates = HTMLprojectDates.replace("%data%", item.dates);
         var Description = HTMLprojectDescription.replace("%data%", item.description);
 
         $(".project-entry:last").append(Title, Dates, Description);
-        item.images.forEach(function (image) {
+        item.images.forEach(function(image) {
             var Image = HTMLprojectImage.replace("%data%", image);
             $(".project-entry:last").append(Image);
         });
